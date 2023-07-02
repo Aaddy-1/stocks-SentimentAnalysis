66b46c383cfb21329c74a6c5060ac07588866980
Aadeesh Sharma <sharmaaadeesh@gmail.com>
2023-07-03 00:16:03 +0530

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import praw
+import os
+from dotenv import load_dotenv
+
+# loading env file
+load_dotenv('environment.env')
+
+
+reddit = praw.Reddit(client_id = os.getenv('CLIENT_ID'),
+                    client_secret = os.getenv('CLIENT_SECRET'),
+                    user_agent = os.getenv('USER_AGENT'))
+
