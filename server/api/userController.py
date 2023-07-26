94989b511eca12d79a087d5249de0f9835c17bf5
Aadeesh Sharma <sharmaaadeesh@gmail.com>
2023-07-26 23:37:50 +0530

    Added scripts for the notebooks and updated data

--- server/api/userController.py
+++ server/api/userController.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 from pathlib import Path
 SERVER_FOLDER = Path(__file__).parent.parent.resolve()
 template_folder = SERVER_FOLDER / "template"
-model_folder = SERVER_FOLDER / "model"
 
 # sys.path.insert(0, model_folder)
 # import model
